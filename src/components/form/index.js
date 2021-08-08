9c98e5e3c4fdfc82154034e6108f193de202b72c
Ibrahim Abu-awad <ibrahimabuawadwork@gmail.com>
2021-08-08 20:31:21 +0300

    done

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,22 +1,56 @@
-import React from 'react';
+// import React from 'react';
 
+// import './form.scss';
+
+// class Form extends React.Component {
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     const formData = {
+//       method:'GET',
+//       url: 'https://pokeapi.co/api/v2/pokemon',
+//     };
+//     this.props.handleApiCall(formData);
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <form onSubmit={this.handleSubmit}>
+//           <label >
+//             <span>URL: </span>
+//             <input name='url' type='text' />
+//             <button type="submit">GO!</button>
+//           </label>
+//           <label className="methods">
+//             <span id="get">GET</span>
+//             <span id="post">POST</span>
+//             <span id="put">PUT</span>
+//             <span id="delete">DELETE</span>
+//           </label>
+//         </form>
+//       </>
+//     );
+//   }
+// }
+
+// export default Form;
+
+import React from 'react';
 import './form.scss';
 
-class Form extends React.Component {
-
-  handleSubmit = e => {
+function Form(props) {
+  function handleSubmit(e) {
     e.preventDefault();
     const formData = {
       method:'GET',
       url: 'https://pokeapi.co/api/v2/pokemon',
     };
-    this.props.handleApiCall(formData);
+    props.handleApiCall(formData);
   }
-
-  render() {
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
+  return (
+    <>
+        <form onSubmit={handleSubmit}>
           <label >
             <span>URL: </span>
             <input name='url' type='text' />
@@ -30,8 +64,8 @@ class Form extends React.Component {
           </label>
         </form>
       </>
-    );
-  }
+  )
 }
 
-export default Form;
+export default Form
+
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
+// import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header>
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header>
+//         <h1>RESTy</h1>
+//       </header>
+//     );
+//   }
+// }
+
+// export default Header;
+
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
         <h1>RESTy</h1>
       </header>
-    );
-  }
+  )
 }
 
-export default Header;
+export default Header
+
